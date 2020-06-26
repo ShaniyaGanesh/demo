bd4904aaf16bbce701cf8b629b052ffef6503b5e
Shaniya M <shaniya@tridentsqa.com>
2020-06-26 14:10:08 +0530

    Shaniya

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class index extends Component {
   render() {
-    return <div>Shaniya Ganesh</div>;
+    return <div>Shaniya Ganesh Kumar</div>;
   }
 }
